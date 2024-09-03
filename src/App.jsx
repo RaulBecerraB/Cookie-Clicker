c434f0153375084963f67cded88da5135eddfea4
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-09-03 16:26:42 -0600

    feat(game.css): add and implement CSS classes for general conteainers

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,17 @@ const Game = () =>
   return(
     <>
     <div>
-      <div className='navBarContainer '>
+      <div className='navbar-container '>
          <h1>Cookie Clicker Game!</h1>
+      </div>
+      <div className='white-board'>
+        <div className='text-container'>
+          <h2>L</h2>
+        </div>
+        <div style={{border:'blue',borderRadius:'20px'}}>
+          <div className='upgrades-container'>
+          </div>
+        </div>
       </div>
     </div>
       
