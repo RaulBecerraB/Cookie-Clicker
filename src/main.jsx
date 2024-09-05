d86bff4996bf904ad01a61fcb3fdef9e3556857b
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-09-05 17:21:28 -0600

    refactor (containers.css): separate container CSS classes into a separate file

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/game.css'
+import './styles/containers.css'
 
 createRoot(document.getElementById('root')).render(<App />)
