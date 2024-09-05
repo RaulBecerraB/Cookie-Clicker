eca7db21838248bc51b7f68b3a608d3f49c78652
DigitalBecerra21 <ingenierobecerrabarcelo@gmail.com>
2024-09-05 17:46:12 -0600

    feat (StoreButton): add cookie SVG to price display

--- src/components/StoreButton.jsx
+++ src/components/StoreButton.jsx
@@ -8,6 +8,7 @@ const StoreButton = ({price,title,onButtonClick}) => {
         </div>
         <div style={{fontSize:'1.0rem',fontWeight:'semi-bold',marginTop:'0.5rem'}}>
           {price}
+          <img src="./src/assets/cookie.svg" alt="cookie" width='15' height='15' style={{marginLeft:'0.5rem',marginBottom:'-0.1rem'}}/>
         </div>
     </button>
   )
